1ad24b96cc2144d4c1e7865c73ed26676fbfac34
horejvi14 <horejvi14@DST20716>
2017-03-15 11:31:36 +0100

    asdfsW

--- Draci doupe/AttackBehavior/EnemyAttackBehavior.cs
+++ Draci doupe/AttackBehavior/EnemyAttackBehavior.cs
@@ -10,8 +10,20 @@ namespace Draci_doupe.AttackBehavior
     {
         public void Attack(Player p, int attackStrenght)
         {
-            p.HP -= attackStrenght;
-            
+            //p.HP -= attackStrenght;
+
+            int nasobitel_utoku = p.Strenght / 10 + 1;
+
+            int delitel_utoku = 1 - p.Strenght / 10;
+
+            int utok_hodnota = p.Attack * nasobitel_utoku * delitel_utoku;
+
+            p.HP -= utok_hodnota;
+
+
+            // strenght = 1 / 10  1 + 0,10
+            //        x /= 10        x += 1
+
 
             //dorseit pocitani se statama a napojeni na random od cube
 
